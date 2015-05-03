b1448c6a58745e7bbf7498e18a2f73499444985c
Sean Enck <enckse@gmail.com>
2015-05-03 09:32:31 -0400

    Update version and make life easier in the future

--- mono.sh
+++ mono.sh
@@ -1,2 +1,3 @@
-wget "http://download.mono-project.com/archive/3.12.1/macos-10-x86/MonoFramework-MDK-3.12.1.macos10.xamarin.x86.pkg"
-sudo installer -pkg "MonoFramework-MDK-3.10.0.macos10.xamarin.x86.pkg" -target /
+VER="4.0.0"
+wget "http://download.mono-project.com/archive/$VER/macos-10-x86/MonoFramework-MDK-$VER.macos10.xamarin.x86.pkg"
+sudo installer -pkg "MonoFramework-MDK-$VER.macos10.xamarin.x86.pkg" -target /
