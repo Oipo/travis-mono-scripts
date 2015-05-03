a552952980b49460f930be38de1224309645c26e
Sean Enck <enckse@gmail.com>
2015-05-03 09:58:33 -0400

    Download mono version for mono and nunit

--- mono.sh
+++ mono.sh
@@ -1,3 +1,4 @@
+wget "https://raw.githubusercontent.com/enckse/travis-mono-scripts/master/mono-version.txt"
 VER=$(cat "mono-version.txt")
 wget "http://download.mono-project.com/archive/$VER/macos-10-x86/MonoFramework-MDK-$VER.macos10.xamarin.x86.pkg"
 sudo installer -pkg "MonoFramework-MDK-$VER.macos10.xamarin.x86.pkg" -target /
--- nunit-wrapper.sh
+++ nunit-wrapper.sh
@@ -1,2 +1,3 @@
+wget "https://raw.githubusercontent.com/enckse/travis-mono-scripts/master/mono-version.txt"
 VER=$(cat "mono-version.txt")
 mono /Library/Frameworks/Mono.framework/Versions/$VER/lib/mono/4.5/nunit-console.exe "$@"
