2904adc8c31f232786af8f942cf16eb94c361a55
Sean Enck <enckse@gmail.com>
2015-05-03 10:04:20 -0400

    Same update for nunit for version

--- nunit-wrapper.sh
+++ nunit-wrapper.sh
@@ -1,3 +1,3 @@
-wget "https://raw.githubusercontent.com/enckse/travis-mono-scripts/master/mono-version.txt"
+wget -O "mono-version.txt" "https://raw.githubusercontent.com/enckse/travis-mono-scripts/master/mono-version.txt"
 VER=$(cat "mono-version.txt")
 mono /Library/Frameworks/Mono.framework/Versions/$VER/lib/mono/4.5/nunit-console.exe "$@"
