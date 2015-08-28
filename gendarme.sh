fc44426d39149e206b3b1be30ec25b0dd2a25b13
Michael de Lang <kingoipo@gmail.com>
2015-08-28 21:28:33 +0200

    Screw wget, glory to curl!

--- gendarme.sh
+++ gendarme.sh
@@ -1,4 +1,4 @@
-wget --secure-protocol=TLSv1 -O gendarme.zip "https://github.com/downloads/spouliot/gendarme/gendarme-2.10-bin.zip"
+curl -L -o gendarme.zip "https://github.com/downloads/spouliot/gendarme/gendarme-2.10-bin.zip"
 sudo mkdir /opt/gendarme
 sudo unzip gendarme.zip -d /opt/gendarme
 sudo echo "mono /opt/gendarme/gendarme.exe \"$@\"" > /usr/local/bin/gendarme
